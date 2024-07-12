02cce17a4582b9b4f7429b5d61dfe81c1a5d5088
farsa <puppetcooder@gmail.com>
2024-07-12 18:20:15 +0300

    add gateway basic impl

---
+++ gateway/gateway.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+
+	orderpb "common/genproto/common/protos/order"
+
+	userpb "common/genproto/common/protos/user"
+
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	mux := runtime.NewServeMux()
+	opts := []grpc.DialOption{grpc.WithInsecure()}
+
+	err := userpb.RegisterUserServiceHandlerFromEndpoint(ctx, mux, "localhost:50051", opts)
+	if err != nil {
+		log.Fatalf("Failed to start HTTP gateway for UserService: %v", err)
+	}
+
+	err = orderpb.RegisterOrderServiceHandlerFromEndpoint(ctx, mux, "localhost:50052", opts)
+	if err != nil {
+		log.Fatalf("Failed to start HTTP gateway for OrderService: %v", err)
+	}
+
+	// Forward /authenticate to AuthService via Traefik
+	mux.HandlePath("POST", "/auth", func(w http.ResponseWriter, r *http.Request, pathParams map[string]string) {
+		http.Redirect(w, r, "http://traefik/authenticate", http.StatusTemporaryRedirect)
+	})
+
+	log.Println("Serving gRPC-Gateway on http://localhost:5000")
+	if err := http.ListenAndServe(":5000", mux); err != nil {
+		log.Fatalf("Failed to serve HTTP gateway: %v", err)
+	}
+}
